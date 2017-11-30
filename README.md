0680a46644086aa2d2fcae2c279e3ff803b863ae
rwart <32432397+rwart@users.noreply.github.com>
2017-11-30 15:44:39 +0100

    ready for review

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 mod-13-7
+
+Save content of current catalog to ./fileList.txt
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-
-/**
-  * Main module
-  * @module index
-  */
+var fs = require('fs');
+fs.readdir('./', function (err, files) {
+  if (err) throw(err);
+  fs.writeFile('./fileList.txt', files.join('\n'), function (err) {
+    if (err) throw(err);
+    console.log('ok');
+  });
+});
